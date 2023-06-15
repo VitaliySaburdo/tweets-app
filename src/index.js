b3680a50dce0e36fb6184d08a8146af75493579b
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-15 18:50:40 +0300

    project recovery

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tweets-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
