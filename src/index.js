6fe3a795c11dba13301dfe3775ea29ae3b0ced30
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-17 09:02:38 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets-app">
+    <BrowserRouter basename="tweets-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
