8ee01b99dcdc0482336d9cad012622f1c5c59eac
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-17 09:47:30 +0300

    12

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="tweets-app">
+    <BrowserRouter basename="/tweets-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
